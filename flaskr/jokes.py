fa94041eeb312a6b4ebc5123efeaa4a745f19b78
idilwor <115816447+idilwor@users.noreply.github.com>
2024-11-14 01:02:00 -0500

    first commit

---
+++ flaskr/jokes.py
@@ -0,0 +1,9 @@
+@bp.route('/')
+def leavejoke():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('jokes/leavejoke.html', posts=posts)
